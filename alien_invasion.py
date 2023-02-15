8d8acf1413b6df16fc4ef458a0f76a6a37bcdca6
Kronivar <t.xristos@hotmail.com>
2023-02-15 22:38:52 +0100

    Alien Invasion up to TIY 13-3

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,7 +29,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self._update_bullets()            
+            self._update_bullets() 
+            self._update_aliens()           
             self._update_screen()
             
             # Make the most recently drawn screen visible.
@@ -108,6 +109,27 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien_height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _update_aliens(self):
+        """
+        Check if the fleet is at an edge,
+            then update the positions of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
+    
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+            self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
